917ef2d13432b36c1ba4211d9b07b56034b3ea10
Brandon xprodeur <brandon-xprodeur@live.fr>
2015-04-29 15:48:42 +0200

    Edit function setFlash

--- SmartLIB/Session.php
+++ SmartLIB/Session.php
@@ -9,11 +9,12 @@ class Session{
      * @param string $type
      */
     public static function setFlash($message,$type = 'success'){
-        var_dump($_SESSION);
         $_SESSION['flash'] = array(
             'message' => $message,
             'type'    => $type
         );
+
+        self:Session::flash();
     }
 
     /**
