ebb8307ec43539e742d58aaf795a3720dc2964af
Brandon xprodeur <brandon-xprodeur@live.fr>
2015-05-03 12:00:37 +0200

    Comment Debug all

--- index.php
+++ index.php
@@ -14,7 +14,7 @@ $app = new Core\Bootstrap();
 
 /* Debug */
 $debug = new Core\Debug();
-$debug->all();
+    // $debug->all();
 
 
 /* Utils */
@@ -31,14 +31,14 @@ $db = new Core\Database('localhost', 'root', '', 'boutique');
 
 
 /* Session */
-$session = new \Core\Session();
+$session = new Core\Session();
     $session->setFlash($ma_variable, 'success');
     $session->write('SmartLIB', $ma_variable);
     $session->read($ma_variable);
 
 
 /* Cache */
-$cache = new \Core\Cache('Cache', '30');
+$cache = new Core\Cache('Cache', '30');
     // $cache->inc('composer.json', 'composer');
     //$cache->write('mon_test.php', $ma_variable);
     //$cache->delete('mon_test');
@@ -47,7 +47,7 @@ $cache = new \Core\Cache('Cache', '30');
 
 /* Form */
 $form = new Core\Form();
-    $form->method('POST');
+    $form->method('GET');
     $form->target('index.php');
     $form->input('text', 'name','Votre nom : ', ''); // 1 = Type | 2 = Name | 3 = Label
     $form->build();
